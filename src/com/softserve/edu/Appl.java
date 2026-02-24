c63d2de9655138ed96eada2eb9dd7fd80a09daf7
bebebe <hahaha1234aaaaa@gmail.com>
2026-02-24 21:04:54 +0200

    Calc added

--- src/com/softserve/edu/Appl.java
+++ src/com/softserve/edu/Appl.java
@@ -4,5 +4,8 @@ public class Appl {
 
     public static void main(String[] args) {
         System.out.println("Hello");
+        //
+        Calc calc = new Calc();
+        System.out.println(" 1 + 2 = " + calc.add(1, 2));
     }
 }
